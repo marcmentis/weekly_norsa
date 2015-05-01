589008286e8688d53b74e55a1298c1eed58b00f8
Marc Mentis <mmentis@optonline.net>
2015-05-01 13:42:52 -0400

    Add session creation

--- app/controllers/concerns/sessionvalues.rb
+++ app/controllers/concerns/sessionvalues.rb
@@ -1,67 +1,64 @@
 module Sessionvalues
-	def test
-		flash[:notice] = "In sessionvalues"
-	end
-
 	def check_session
 		if session[:authen].blank?
 			create_session
 		end
 	end
 
-	def create_session
+	private
+		def create_session
 
-		# Get 'request' and 'response' objects from Authentication system
-	    @request = request
-	    @response = response
-	    
-	    # Raise a NoMethodError if user not in Users database
-	    # ? Differentiate between 'development' and 'production' for how this is done
-	    if @request.headers["HTTP_REMOTE_USER"].blank?
-		      # Artificially set session
-		      begin
-		        session[:authen] = 'pgmdmjm'
-		        @user = User.where('authen = ?', session[:authen]).first
-		        session[:facility] = @user.facility
-		        session[:email] = @user.email
-		        session[:firstname] = @user.firstname
-		        session[:lastname] = @user.lastname
-		        session[:firstinitial] = @user.firstinitial
-		        session[:middleinitial] = @user.middleinitial
-		        session[:name] = ''+@user.firstinitial+' '+@user.middleinitial+' '+@user.lastname+''
-		        session[:role] = @user.role
+			# Get 'request' and 'response' objects from Authentication system
+		    @request = request
+		    @response = response
+		    
+		    # Raise a NoMethodError if user not in Users database
+		    # ? Differentiate between 'development' and 'production' for how this is done
+		    if @request.headers["HTTP_REMOTE_USER"].blank?
+			      # Artificially set session
+			      begin
+			        session[:authen] = 'pgmdmjm'
+			        @user = User.where('authen = ?', session[:authen]).first
+			        session[:facility] = @user.facility
+			        session[:email] = @user.email
+			        session[:firstname] = @user.firstname
+			        session[:lastname] = @user.lastname
+			        session[:firstinitial] = @user.firstinitial
+			        session[:middleinitial] = @user.middleinitial
+			        session[:name] = ''+@user.firstinitial+' '+@user.middleinitial+' '+@user.lastname+''
+			        session[:role] = @user.role
 
-		        # @for_select = ForSelect.where('value = ?', session[:facility]).first
-		        # session[:facilityname] = @for_select.text
-		      rescue NoMethodError
-		        render file: "#{Rails.root}/public/user_error", layout: false
-		      end
-	      
-	    else
-			begin
-			session[:authen] = @request.headers["HTTP_REMOTE_USER"]
-			@user = User.where('authen = ?', session[:authen]).first
-			# session[:facility] = @request.headers["HTTP_OMHFACILITYNUM"]
-			# session[:email] = @request.headers["HTTP_CTEMAIL"]
-			# session[:firstname] = @request.headers["HTTP_CTFN"]
-			# session[:lastname] = @request.headers["HTTP_CTLN"]
-			# session[:firstinitial] = @request.headers["HTTP_CTFN"]
-			# session[:middleinitial] = @request.headers["HTTP_INITIALS"]
-			session[:facility] = @user.facility
-			session[:email] = @user.email
-			session[:firstname] = @user.firstname
-			session[:lastname] = @user.lastname
-			session[:firstinitial] = @user.firstinitial
-			session[:middleinitial] = @user.middleinitial
-			session[:name] = ''+@user.firstinitial+' '+@user.middleinitial+' '+@user.lastname+''
-			session[:role] = @user.role
+			        # @for_select = ForSelect.where('value = ?', session[:facility]).first
+			        # session[:facilityname] = @for_select.text
+			      rescue NoMethodError
+			        render file: "#{Rails.root}/public/user_error", layout: false
+			      end
+		      
+		    else
+				begin
+				session[:authen] = @request.headers["HTTP_REMOTE_USER"]
+				@user = User.where('authen = ?', session[:authen]).first
+				# session[:facility] = @request.headers["HTTP_OMHFACILITYNUM"]
+				# session[:email] = @request.headers["HTTP_CTEMAIL"]
+				# session[:firstname] = @request.headers["HTTP_CTFN"]
+				# session[:lastname] = @request.headers["HTTP_CTLN"]
+				# session[:firstinitial] = @request.headers["HTTP_CTFN"]
+				# session[:middleinitial] = @request.headers["HTTP_INITIALS"]
+				session[:facility] = @user.facility
+				session[:email] = @user.email
+				session[:firstname] = @user.firstname
+				session[:lastname] = @user.lastname
+				session[:firstinitial] = @user.firstinitial
+				session[:middleinitial] = @user.middleinitial
+				session[:name] = ''+@user.firstinitial+' '+@user.middleinitial+' '+@user.lastname+''
+				session[:role] = @user.role
 
-			@for_select = ForSelect.where('value = ?', session[:facility]).first
-			session[:facilityname] = @for_select.text
-			rescue NoMethodError
-			    render file: "#{Rails.root}/public/user_error", layout: false
-			end
-	 	end
-	end
+				@for_select = ForSelect.where('value = ?', session[:facility]).first
+				session[:facilityname] = @for_select.text
+				rescue NoMethodError
+				    render file: "#{Rails.root}/public/user_error", layout: false
+				end
+		 	end
+		end
 	
 end
