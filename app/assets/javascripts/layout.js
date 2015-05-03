c2ae97c3f54c354ebf3ac93af911741e4bc10a17
Marc Mentis <mmentis@optonline.net>
2015-05-03 19:16:27 -0400

    Start css structuring

---
+++ app/assets/javascripts/layout.js
@@ -0,0 +1,9 @@
+$(function(){
+	// Load the superfish horizontal navigation menu
+	jQuery('ul.sf-menu').superfish();
+
+	// STYLE 
+	$('#fig').addClass('float_left');
+	$('#clear_break').addClass('clear_both');
+	$('#header').addClass('header')
+});
