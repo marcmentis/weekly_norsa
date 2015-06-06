69cb916f6b5f866f4a266d7515b51b541078578b
Marc Mentis <mmentis@optonline.net>
2015-06-06 19:32:01 -0400

    Authentication wip

--- app/controllers/concerns/session_values.rb
+++ app/controllers/concerns/session_values.rb
@@ -31,6 +31,7 @@ module SessionValues
 		# elsif Rails.env == 'development' || Rails.env == 'production'
 		elsif Rails.env == 'production'
 			if rfc_authorized? 
+				session[:authen] = request.headers["HTTP_REMOTE_USER"]
 				@current_user = current_user
 				if @current_user.blank?
 					@error = 'User has no privileges in this application'
@@ -44,61 +45,6 @@ module SessionValues
 	end
 
 	private
-		# def create_session
-
-		# 	# Get 'request' and 'response' objects from Authentication system
-		#     @request = request
-		#     @response = response
-		    
-		#     # Raise a NoMethodError if user not in Users database
-		#     # ? Differentiate between 'development' and 'production' for how this is done
-		#     if @request.headers["HTTP_REMOTE_USER"].blank?
-		# 	      # Artificially set session
-		# 	      begin
-		# 	        session[:authen] = 'pgmdmjm'
-		# 	        @user = User.find_by(authen: session[:authen])
-		# 	        session[:facility] = @user.facility
-		# 	        session[:email] = @user.email
-		# 	        session[:firstname] = @user.firstname
-		# 	        session[:lastname] = @user.lastname
-		# 	        session[:firstinitial] = @user.firstinitial
-		# 	        session[:middleinitial] = @user.middleinitial
-		# 	        session[:name] = ''+@user.firstinitial+' '+@user.middleinitial+' '+@user.lastname+''
-			       
-
-		# 	        # @for_select = ForSelect.where('value = ?', session[:facility]).first
-		# 	        # session[:facilityname] = @for_select.text
-		# 	      rescue NoMethodError
-		# 	        render file: "#{Rails.root}/public/user_error", layout: false
-		# 	      end
-		      
-		#     else
-		# 		begin
-		# 		session[:authen] = @request.headers["HTTP_REMOTE_USER"]
-		# 		@user = User.find_by(authen: session[:authen])
-		# 		# session[:facility] = @request.headers["HTTP_OMHFACILITYNUM"]
-		# 		# session[:email] = @request.headers["HTTP_CTEMAIL"]
-		# 		# session[:firstname] = @request.headers["HTTP_CTFN"]
-		# 		# session[:lastname] = @request.headers["HTTP_CTLN"]
-		# 		# session[:firstinitial] = @request.headers["HTTP_CTFN"]
-		# 		# session[:middleinitial] = @request.headers["HTTP_INITIALS"]
-		# 		session[:facility] = @user.facility
-		# 		session[:email] = @user.email
-		# 		session[:firstname] = @user.firstname
-		# 		session[:lastname] = @user.lastname
-		# 		session[:firstinitial] = @user.firstinitial
-		# 		session[:middleinitial] = @user.middleinitial
-		# 		session[:name] = ''+@user.firstinitial+' '+@user.middleinitial+' '+@user.lastname+''
-
-
-		# 		@for_select = ForSelect.where('value = ?', session[:facility]).first
-		# 		session[:facilityname] = @for_select.text
-		# 		rescue NoMethodError
-		# 		    render file: "#{Rails.root}/public/user_error", layout: false
-		# 		end
-		#  	end
-		# end
-
 		def rfc_authorized?
 	  		if request.headers["HTTP_REMOTE_USER"].blank?
 	  			return false
@@ -119,6 +65,61 @@ module SessionValues
 			
 			@_current_user ||= session[:authen] && User.find_by(authen: session[:authen])
 			# @_current_user ||= User.find_by(authen: 'pgmdmjm')
+		end
+
+		def create_session
+
+			# # Get 'request' and 'response' objects from Authentication system
+		 #    @request = request
+		 #    @response = response
+		    
+		 #    # Raise a NoMethodError if user not in Users database
+		 #    # ? Differentiate between 'development' and 'production' for how this is done
+		 #    if @request.headers["HTTP_REMOTE_USER"].blank?
+			#       # Artificially set session
+			#       begin
+			#         session[:authen] = 'pgmdmjm'
+			#         @user = User.find_by(authen: session[:authen])
+			#         session[:facility] = @user.facility
+			#         session[:email] = @user.email
+			#         session[:firstname] = @user.firstname
+			#         session[:lastname] = @user.lastname
+			#         session[:firstinitial] = @user.firstinitial
+			#         session[:middleinitial] = @user.middleinitial
+			#         session[:name] = ''+@user.firstinitial+' '+@user.middleinitial+' '+@user.lastname+''
+			       
+
+			#         # @for_select = ForSelect.where('value = ?', session[:facility]).first
+			#         # session[:facilityname] = @for_select.text
+			#       rescue NoMethodError
+			#         render file: "#{Rails.root}/public/user_error", layout: false
+			#       end
+		      
+		 #    else
+			# 	begin
+			# 	session[:authen] = @request.headers["HTTP_REMOTE_USER"]
+			# 	@user = User.find_by(authen: session[:authen])
+			# 	# session[:facility] = @request.headers["HTTP_OMHFACILITYNUM"]
+			# 	# session[:email] = @request.headers["HTTP_CTEMAIL"]
+			# 	# session[:firstname] = @request.headers["HTTP_CTFN"]
+			# 	# session[:lastname] = @request.headers["HTTP_CTLN"]
+			# 	# session[:firstinitial] = @request.headers["HTTP_CTFN"]
+			# 	# session[:middleinitial] = @request.headers["HTTP_INITIALS"]
+			# 	session[:facility] = @user.facility
+			# 	session[:email] = @user.email
+			# 	session[:firstname] = @user.firstname
+			# 	session[:lastname] = @user.lastname
+			# 	session[:firstinitial] = @user.firstinitial
+			# 	session[:middleinitial] = @user.middleinitial
+			# 	session[:name] = ''+@user.firstinitial+' '+@user.middleinitial+' '+@user.lastname+''
+
+
+			# 	@for_select = ForSelect.where('value = ?', session[:facility]).first
+			# 	session[:facilityname] = @for_select.text
+			# 	rescue NoMethodError
+			# 	    render file: "#{Rails.root}/public/user_error", layout: false
+			# 	end
+		 # 	end
 		end
 	
 end
