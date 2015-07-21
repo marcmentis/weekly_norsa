1c0d67ce874e32a4fe4984d3ebb24ba702e71a38
Marc Mentis <mmentis@optonline.net>
2015-07-21 17:02:20 -0400

    Start HTML / CSS

---
+++ app/assets/javascripts/mx_assessments.js
@@ -0,0 +1,17 @@
+$(function(){
+if ($('body.mx_assessments').length) {
+
+	// STYLING
+		$('#divMxAssessPageWrapper').addClass('pad_3_sides');
+		$('#divMxAssessPageInnerWrapper').addClass('centered')
+										.css({'width':'75em',
+										'background-color': 'red'});
+		$('#divMxAssessRtContainer').addClass('float_right form_container')
+									.css({'width': '36em',
+									'display':'inline',});
+		$('#divMxAssessLtContainer').addClass('float_left form_container')
+									.css({'width':'36em'});
+
+
+};	//if ($('body.mx_assessments').length) {
+});  //$(function(){
