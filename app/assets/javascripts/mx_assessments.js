94f348b4a1a646d97594e92240c11a3d91c7b7c0
Marc Mentis <mmentis@optonline.net>
2015-07-22 17:38:34 -0400

    Mx_Assessment CSS wip

--- app/assets/javascripts/mx_assessments.js
+++ app/assets/javascripts/mx_assessments.js
@@ -1,16 +1,74 @@
 $(function(){
 if ($('body.mx_assessments').length) {
 
+	//VARIABLES
+		//textareas
+		var width1 = '40em'
+		var heightS1 = '20em';
+		var heightL1 = '35em';
+		var heightEL1 = '55em';
+
 	// STYLING
-		$('#divMxAssessPageWrapper').addClass('pad_3_sides');
-		$('#divMxAssessPageInnerWrapper').addClass('centered')
-										.css({'width':'75em',
-										'background-color': 'red'});
-		$('#divMxAssessRtContainer').addClass('float_right form_container')
-									.css({'width': '36em',
-									'display':'inline',});
-		$('#divMxAssessLtContainer').addClass('float_left form_container')
-									.css({'width':'36em'});
+		//divs
+		$('#divMxAssessPageWrapper')
+			.addClass('pad_3_sides');
+		$('#divMxAssessPageInnerWrapper')
+			.addClass('centered')
+			.css({'width':'75em',
+				'overflow': 'hidden'});
+		$('#divTitle')
+			.css({'text-align': 'center',
+					'color': '#2e6e9e',
+					'font-size': '17px',
+					'font-weight': 'bold',
+					'text-align': 'center',
+					'margin': '0 0 7px 0'})
+		$('#divMxAssessRtContainer')
+			.addClass('float_right form_container')
+			.css({'width': '36em'});
+		$('#divMxAssessLtContainer')
+			.addClass('float_left form_container')
+			.css({'width':'36em'});
+		$('#divMxAssessToDo')
+			.addClass('float_left');
+		$('#divMxAssessDone')
+			.addClass('float_right');
+		$('#divMxAssessLtMiddle')
+			.css({'overflow': 'hidden'})
+		$('#divMxAssessLtBottom')
+			.addClass('clear_both');
+		$('#divMxAssessLtTop, #divMxAssessLtMiddle')
+			.css({'margin-bottom': '1em'})
+
+		//dates
+		$('[id^=dt]')
+			.datepicker({
+				changeMonth: true,
+				changeYear: true,
+				yearRange: "-10: +10" })
+			.addClass('texts')
+			.css({'width':'7em'});
+
+		//selects
+		$('#slt_MxA_to_do, #slt_MxA_done')
+			.attr({'size': '15',
+				'multiple': 'no'})
+			.css({'width': '18em',
+				'overflow': 'hidden'});
+		//buttons
+		$('[id^=bt]')
+			.button();
+		$('#btTogNotes')
+			.addClass('reduce_button')
+			.attr({'value': 'toggle'});
+		//textareas
+		$('[id^=txa')
+			.addClass('texts');
+		$('#txa_past_MxAssessments')
+			.width(width1)
+			.height(heightS1)
+			
+
 
 
 };	//if ($('body.mx_assessments').length) {
