0ff04b4693657636be13b70ae219ec50150587cf
Marc Mentis <mmentis@optonline.net>
2015-09-08 14:49:19 -0400

    authorization refactor wip

--- app/policies/mxa_tracker_policy.rb
+++ app/policies/mxa_tracker_policy.rb
@@ -1,18 +1,18 @@
 class MxaTrackerPolicy < Struct.new(:current_user, :mxa_tracker)
-	attr_reader :current_user, :mxa_tracker
+	# attr_reader :current_user, :mxa_tracker
 
-	def initialize(current_user, mxa_tracker)
-		@current_user = current_user
-		@mxa_tracker = mxa_tracker
-	end
+	# def initialize(current_user, mxa_tracker)
+	# 	@current_user = current_user
+	# 	@mxa_tracker = mxa_tracker
+	# end
 
 	def index?
-		@current_user.has_role? :admin3 or
-		@current_user.has_role? :admin2 or  
-		@current_user.has_role? :trackers_r or
+		current_user.has_role? :admin3 or
+		current_user.has_role? :admin2 or  
+		current_user.has_role? :trackers_r or
 		(			
-			@current_user.has_role? :trash1 and 
-			@current_user.has_role? :trash2
+			current_user.has_role? :trash1 and 
+			current_user.has_role? :trash2
 		)
 	end
 end
--- app/policies/widget_policy.rb
+++ app/policies/widget_policy.rb
@@ -1,12 +1,12 @@
 class WidgetPolicy < Struct.new(:current_user, :widget)
-	attr_reader :current_user, :widgets
+	# attr_reader :current_user, :widgets
 
-	def initialize(current_user, widget)
-		@current_user = current_user
-		@widget = widgets
-	end
+	# def initialize(current_user, widget)
+	# 	@current_user = current_user
+	# 	@widget = widgets
+	# end
 
 	def index?
-		@current_user.has_role? :r_and_d
+		current_user.has_role? :r_and_d
 	end
 end
